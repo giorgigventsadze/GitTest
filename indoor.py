16370d88572e3e86461debb834d92128bf15ecd1
giorgigventsadze <60110035+giorgigventsadze@users.noreply.github.com>
2023-04-24 15:49:19 +0400

    Update indoor.py

--- indoor.py
+++ indoor.py
@@ -2,3 +2,9 @@ name = input("What's yor name? ")
 print ("Hello", name.lower())
 surname = input("What's your surname? ")
 print ("Nice to meet you! ", name.lower(), surname.lower())
+
+
+
+name = "GIORGI"
+surname = "GVENTSADZE"
+print ("Hello ,Nice to meet you! ", name.lower(), surname.lower())
